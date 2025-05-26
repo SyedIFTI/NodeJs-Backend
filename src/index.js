74d68ceb14b37b51af70d20808df38b0ff56fc95
Ali <syedifti0245@gmail.com>
2025-05-26 01:11:28 -0700

    Work on Utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv'
 import DbConnection from "./db/db_connection.js";
+import app from './app.js';
 dotenv.config({
     path:"./env"
 })
-DbConnection()
+DbConnection().then(()=>{
+    app.on('error',(error)=>{
+console.log("ERRR: ",error)
+throw error
+    })
+    app.listen(process.env.PORT||8000 , ()=>{
+        console.log(`Server is running on PORT ${process.env.PORT}`)
+    })
+}).catch((error)=>{
+    console.log('DB CONNECTION ERROR !!!!', error)
+})
